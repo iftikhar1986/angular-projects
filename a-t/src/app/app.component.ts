a9d8811fde9ec926ee7a4b1d8b8c105f336d7879
iftikhar <iftikhar.uddin.fazal@gmail.com>
2025-02-21 22:58:40 +0300

    component created

--- a-t/src/app/app.component.ts
+++ a-t/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from "./components/add-employee/add-employee.component";
+import { AddListComponent } from "./components/add-list/add-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployeeComponent, AddListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ a-t/src/app/components/add-list/add-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-list',
+  imports: [],
+  template: '<h1>Add List</h1>',
+  styleUrl: './add-list.component.css'
+})
+export class AddListComponent {
+
+}
